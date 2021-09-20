8906725b5ed3eff333047f666def84ce81c885cc
Danny <danjmurphy92@gmail.com>
2021-09-20 17:57:23 +0100

    Implemented cli

--- src/Palindrome/Program.cs
+++ src/Palindrome/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Palindrome.services;
+using System;
 
 namespace Palindrome
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var palindromeSvc = new PalindromeService();
+
+            Console.WriteLine("Welcome to your Palindrome input tester");
+            Console.WriteLine("Enter in the text you wish to test, or enter \"EXIT\" to quit (its not a palindrome, I've checked)");
+            var input = Console.ReadLine();
+            while (input != "EXIT")
+            { 
+                var result = palindromeSvc.CanFormPalindrome(input);
+
+                Console.WriteLine(result ? "TRUE" : "FALSE");
+                input = Console.ReadLine();
+            } 
         }
     }
 }

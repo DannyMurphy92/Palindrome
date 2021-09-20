96aa6230987e11882bbcacf2652ec25c353801cb
Danny <danjmurphy92@gmail.com>
2021-09-20 17:39:58 +0100

    First pass at testing can a palindrome be created

---
+++ src/Palindrome/services/PalindromeService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome.services
+{
+    public interface IPalindromeService
+    {
+        bool CanFormPalindrome(string input);
+    }
+    public class PalindromeService : IPalindromeService
+    {
+        public bool CanFormPalindrome(string input)
+        {
+            var characterDictionary = new Dictionary<char, int>();
+
+            var characters = input.ToCharArray();
+
+            for(int i = 0; i < characters.Length; i++)
+            {
+                var character = characters[i];
+
+                if (characterDictionary.ContainsKey(character))
+                {
+                    characterDictionary[character]++;
+                }else
+                {
+                    characterDictionary[character] = 1;
+                }
+            }
+
+            var oddCharacters = characterDictionary.Count(entry => entry.Value % 2 == 1);
+
+            // If input is even a valid palindrome cannot be formed if there are any odd characters
+            // Conversely if input has an odd length there must be 1 odd character to "pivot" the palindrome around
+            if(characters.Length % 2  == 0)
+            {
+                return oddCharacters == 0;
+            }
+
+            return oddCharacters == 1;
+        }
+    }
+}
---
+++ tests/Palindrome.UnitTests/services/PalindromeServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using Palindrome.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Palindrome.UnitTests.services
+{
+    public class PalindromeServiceTests
+    {
+        [Theory]
+        [InlineData("racecar")]
+        [InlineData("carrace")]
+        public void GivenAPalindromableStringWithNoWhitespace_CorrectlyIdentifiesAsPalindrome(string input)
+        {
+            var sut = new PalindromeService();
+
+            Assert.True(sut.CanFormPalindrome(input));
+        }
+    }
+}
